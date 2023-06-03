14e7aada3cbc8c7d58c7f21277b6caac0071d22a
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2023-06-03 12:24:27 -0700

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { authenticateUser, authorizePermissions } = require('../middleware/authentication');
 
+/*
+    This code is defines routes for the application.
+    The first route is for creating an order, and the second route is for getting all orders.
+    The third route is for getting a specific order, 
+    and the fourth route is for getting the current user's orders. 
+    The fifth route is for updating an order. 
+    All of these routes require authentication and authorization before they can be accessed.
+*/
 const {
     createOrder,
     getAllOrders,
