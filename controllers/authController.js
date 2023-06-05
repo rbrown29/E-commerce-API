f2ef3c5f2c2819ee8e9e4f8a563c3f363d04b83f
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2023-06-05 14:52:36 -0700

    update auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,14 @@ const CustomError = require('../errors');
 const { attachCookiesToResponse, createTokenUser } = require('../utils');
 
 
-
+/*
+    This code is a function that is used to register a new user. 
+    It checks if the email address provided already exists, 
+    and if it does, it throws an error. If the email address does not exist, 
+    it creates a new user with the provided email, name, and password. 
+    It then creates a token for the user and attaches cookies to the response. 
+    It sends back a status code of "created" and returns the token user in JSON format.
+*/
 const register = async (req, res) => {
     const { email, name, password } = req.body;
     const emailAlreadyExists = await User.findOne({ email });
@@ -18,6 +25,14 @@ const register = async (req, res) => {
     res.status(StatusCodes.CREATED).json({ user: tokenUser });
 }
 
+/*
+    This code is a login function that checks if the provided email and password are valid. 
+    It first checks if an email and password were provided, 
+    then looks for a user with the given email in the database. 
+    If a user is found, it compares the provided password to the one stored in the database. 
+    If they match, it creates a token for the user and attaches it to the response. 
+    Finally, it sends an OK status code with the tokenized user back to the client.
+*/
 const login = async (req, res) => {
     const { email, password } = req.body;
 
@@ -39,6 +54,12 @@ const login = async (req, res) => {
     res.status(StatusCodes.OK).json({ user: tokenUser });
 }
 
+/*
+    This code is setting a cookie with the token 'logout' and 
+    setting the expiration date to the current time. 
+    It is also setting the secure flag to true if the environment is set to production. 
+    Finally, it is sending a response with a status code of OK and a message that the user has been logged out.
+*/
 const logout = async (req, res) => {
     res.cookie('token', 'logout', {
         httpOnly: true,
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+/*
+    This code is setting up routes for a user authentication. 
+    The first line is importing the authentication controller functions, register, login, and logout. 
+    The following lines are setting up routes for each of those functions, 
+    with the HTTP method (POST or GET) and the corresponding controller function.
+*/
 const {
     register,
     login,
