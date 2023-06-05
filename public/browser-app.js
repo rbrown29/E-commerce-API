59f485747598c1ba3fc1447b28dad7cfb1bb13db
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2023-06-05 11:32:49 -0700

    update

--- public/browser-app.js
+++ public/browser-app.js
@@ -7,13 +7,13 @@
         if (IsJsonString(html)) { var obj = JSON.parse(html); var formattedJson = JSON.stringify(obj, null, 4); ctx.html("<pre>" + syntaxHighlight(formattedJson) + "</pre>"); } else { ctx.html("<pre>" + escapeHtml(html) + "</pre>"); }
     }); $(".resp-selector").change(function () { $(this).find("option").map(function () { $("#" + this.value).hide() }); var $option = $(this).find('option:selected'); $("#" + $option.val()).show() }); $('[data-toggle="tooltip"]').tooltip();
 }); function escapeHtml(text) { var map = { '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#039;' }; return text.replace(/[&<>"']/g, function (m) { return map[m]; }); }
-        function IsJsonString(str) {
-            try { JSON.parse(str); } catch (e) { return false; }
-            return true;
-        }
-        String.prototype.replaceAll = function (replaceThis, withThis) { var re = new RegExp(RegExp.quote(replaceThis), "g"); return this.replace(re, withThis); }; RegExp.quote = function (str) { return str.replace(/([.?*+^$[\]\\(){}-])/g, "\\$1"); }; function syntaxHighlight(json) {
-            json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;'); return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-                var cls = 'number'; if (/^"/.test(match)) { if (/:$/.test(match)) { cls = 'key'; } else { cls = 'string'; } } else if (/true|false/.test(match)) { cls = 'boolean'; } else if (/null/.test(match)) { cls = 'null'; }
-                return '<span class="' + cls + '">' + match + '</span>';
-            });
-        }
+function IsJsonString(str) {
+    try { JSON.parse(str); } catch (e) { return false; }
+    return true;
+}
+String.prototype.replaceAll = function (replaceThis, withThis) { var re = new RegExp(RegExp.quote(replaceThis), "g"); return this.replace(re, withThis); }; RegExp.quote = function (str) { return str.replace(/([.?*+^$[\]\\(){}-])/g, "\\$1"); }; function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;'); return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number'; if (/^"/.test(match)) { if (/:$/.test(match)) { cls = 'key'; } else { cls = 'string'; } } else if (/true|false/.test(match)) { cls = 'boolean'; } else if (/null/.test(match)) { cls = 'null'; }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
