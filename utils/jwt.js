6532f9f5f192bdf297f92aaf6ef48087a07ffbac
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2023-06-11 15:15:47 -0700

    Update jwt.js

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,27 +1,50 @@
 const jwt = require('jsonwebtoken');
 
-const createJWT = ({payload}) => {
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFETIME});
-    return token;
-}
+const createJWT = ({ payload }) => {
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
+  return token;
+};
 
-const isTokenValid = ({token}) => jwt.verify(token, process.env.JWT_SECRET);
+const isTokenValid = ( token ) => jwt.verify(token, process.env.JWT_SECRET);
 
-const attachCookiesToResponse = ({res, user}) => {
-    const token = createJWT({payload: user});
-    const oneDay = 1000 * 60 * 60 * 24;
-    res.cookie('token', token, {
-        httpOnly: true,
-        expires: new Date(Date.now() + oneDay),
-        secure: process.env.NODE_ENV === 'production',
-        signed: true,
-    });
-}
+const attachCookiesToResponse = ({ res, user, refreshToken }) => {
+  const accessTokenJWT = createJWT({ payload: { user } });
+  const refreshTokenJWT = createJWT({ payload: { user, refreshToken } });
+
+  const oneDay = 1000 * 60 * 60 * 24;
+  const longerExp = 1000 * 60 * 60 * 24 * 30;
+
+  res.cookie('accessToken', accessTokenJWT, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    signed: true,
+    expires: new Date(Date.now() + oneDay),
+  });
+
+  res.cookie('refreshToken', refreshTokenJWT, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    signed: true,
+    expires: new Date(Date.now() + longerExp),
+  });
+};
+
+// keep this for rollback if needed
+const attachSingleCookieToResponse = ({ res, user }) => {
+  const token = createJWT({ payload: user });
+
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+    signed: true,
+  });
+};
 
 module.exports = {
-    createJWT,
-    isTokenValid,
-    attachCookiesToResponse,
-}
-
-
+  createJWT,
+  isTokenValid,
+  attachCookiesToResponse,
+};
