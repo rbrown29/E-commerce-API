a09c69c4e56c4ed4d89367553ff561c655660cee
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2023-06-11 15:18:48 -0700

    Update authentication.js

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,32 +1,50 @@
 const CustomError = require('../errors');
-const user = require('../models/user');
 const { isTokenValid } = require('../utils');
+const Token = require('../models/token');
+const { attachCookiesToResponse } = require('../utils');
 
 const authenticateUser = async (req, res, next) => {
-    const token = req.signedCookies.token;
-    if (!token) {
-        throw new CustomError.UnauthenticatedError('Authentication invalid');
-    } 
-    try {
-        const {name, userId, role} = isTokenValid({token});
-        req.user = {
-            name,
-            userId,
-            role,
-        }
-        next();
-    } catch (error) {
-        throw new CustomError.UnauthenticatedError('Authentication invalid');
+  const { refreshToken, accessToken } = req.signedCookies;
+
+  try {
+   if (accessToken) {
+      const payload = isTokenValid(accessToken);
+      req.user = payload.user;
+      return next();
     }
-}
+    const payload = isTokenValid(refreshToken);
+    const existingToken = await Token.findOne({
+      user : payload.user.userId,
+      refreshToken : payload.refreshToken,
+    });
+    if (!existingToken || !existingToken?.isValid) {
+      throw new CustomError.UnauthenticatedError('Invalid Credentials');
+    }
+    attachCookiesToResponse({ 
+      res, 
+      user: payload.user, 
+      refreshToken: existingToken.refreshToken, 
+    });
+    req.user = payload.user;
+    next();
+   }
+   catch (error) {
+    throw new CustomError.UnauthenticatedError('Authentication Invalid');
+  }
+};
 
 const authorizePermissions = (...roles) => {
-    return (req, res, next) => {
-        if (!roles.includes(req.user.role)) {
-            throw new CustomError.UnauthorizedError('Unauthorized to access this route');
-        }
-        next();
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError(
+        'Unauthorized to access this route'
+      );
     }
-}
+    next();
+  };
+};
 
-module.exports = { authenticateUser, authorizePermissions };
+module.exports = {
+  authenticateUser,
+  authorizePermissions,
+};
